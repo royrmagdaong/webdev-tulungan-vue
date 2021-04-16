e061945bbc136b9d08cbb531364240d4b1b421da
roymagdaong <roy@bisonwolf.com>
2021-04-17 00:03:15 +0800

    add paragraph

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <h1>hello world from web dev tulungan</h1>
+    <p>
+      welcome to web dev tulungan, matututo ka kapag nag aral ka
+    </p>
   </div>
 </template>
 
